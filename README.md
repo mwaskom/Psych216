f021efb18c26ce13c1d109be0acb40dc465e0ae5
Michael Waskom <mwaskom@stanford.edu>
2012-06-20 11:19:55 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Psych 216A: Statistics and data analysis in <strike>MATLAB</strike> Python
+==========================================================================
+
+A translation of the Psych216 homeworks from MATLAB to Python. Why? I'm not sure.
---
+++ utils.py
@@ -0,0 +1,20 @@
+import numpy as np
+from scipy import stats
+
+
+def bootstrap(a, n_boot=10000, stat_func=np.mean):
+
+    boot_dist = np.zeros(n_boot)
+    n = len(a)
+    for i in xrange(n_boot):
+        sample = a[np.random.randint(0, n, n)]
+        boot_dist[i] = stat_func(sample)
+    return boot_dist
+
+
+def prctiles(a, pcts):
+
+    out = np.zeros(len(pcts))
+    for i, p in enumerate(pcts):
+        out[i] = stats.scoreatpercentile(a, p)
+    return out
